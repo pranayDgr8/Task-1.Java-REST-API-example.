4788ce1b7aac066115dc1d5d41beed1b10c3bb5b
Pranaydeep Mayank <63444224+pranayDgr8@users.noreply.github.com>
2023-03-30 12:38:32 +0530

    Add files via upload

---
+++ src/test/java/com/java_api_crud/KaiburrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java_api_crud.kaiburr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaiburrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
